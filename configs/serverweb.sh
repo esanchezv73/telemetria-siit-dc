f4946680aa21efe0f8a07004aa171bfe2c1d5c9c
Ernesto Sánchez <154641197+ernestosv73@users.noreply.github.com>
2024-09-26 22:34:48 -0300

    Update serverweb.sh

--- configs/serverweb.sh
+++ configs/serverweb.sh
@@ -1,20 +1,11 @@
 apk add openrc 
 sleep 1
-apk add apache2
-#apk add nginx
+apk add nginx
 sleep 1
 apk add openssl
 sleep 1
 touch /run/openrc/softlevel
 sleep 1
-#/etc/rc.d/apache2 stop
 #/etc/init.d/nginx stop
 #/usr/sbin/httpd
-# Crear un directorio para los certificados SSL
-mkdir -p /etc/apache2/ssl
 
-# Generar un certificado SSL auto-firmado
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout /etc/apache2/ssl/apache.key \
-  -out /etc/apache2/ssl/apache.crt \
-  -subj "/C=AR/ST=Salta/L=Salta/O=Ucasal/OU=Unit/CN=dominio.com"
